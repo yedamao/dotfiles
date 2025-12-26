2359663cea1512186f4aa18541aa1f464b850e2f
yedamao <logindaveye@gmail.com>
2025-12-26 13:56:05 +0800

    Update nvim and ghostty
    ghostty:
    - add font config
    
    nvim:
    - add auto-session
    - add indent-blackline
    - add more colorscheme

--- dot_config/nvim/lua/plugins/colorscheme.lua
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -10,4 +10,18 @@ return {
 		priority = 1000,
 		opts = {},
 	},
+	{
+		"folke/tokyonight.nvim",
+		lazy = false,
+		priority = 1000,
+		opts = {},
+	},
+	{
+		"Tsuzat/NeoSolarized.nvim",
+		lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+			vim.cmd [[ colorscheme NeoSolarized ]]
+    end
+	},
 }
